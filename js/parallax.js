59ab4bf4f0ce3faad6a2a3a07910064e66946a2f
Luis Parra Gomez <ljpgluisjop@gmail.com>
2020-07-10 10:52:24 -0400

    se agregan fuentes para generar conflicto

--- js/parallax.js
+++ js/parallax.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$(window).scroll(function(){
 		var barra = $(window).scrollTop();
+        # lo que va entrando de master
 		var posicion =  (barra * 0.15);
 		$('.cover').css({
 			'background-position': '0 -' + posicion + 'px'
