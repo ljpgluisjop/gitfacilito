216e42148066648d34828565236b22963cab0322
Luis Parra Gomez <ljpgluisjop@gmail.com>
2020-07-10 10:46:12 -0400

    se agregan devminor

--- js/parallax.js
+++ js/parallax.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$(window).scroll(function(){
 		var barra = $(window).scrollTop();
+		#hola estto es una prueba devminor
 		var posicion =  (barra * 0.15);
 		$('.cover').css({
 			'background-position': '0 -' + posicion + 'px'
